589460f97d4a94daf6011a3bac731673e741ec91
EngineeringSoluTech <engineringsolutech@gmail.com>
2024-04-13 21:14:35 -0500

    subida del proyecto

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()  #llamamos una clase de fastapi es un contexto inicial
+
+#def es para definir una funcion
+# con @app. accedemos al contexto app y podemos comunicar ocn las rutas de la app
+@app.get('/')  
+async def root():  
+    return "hola mundo"
+
+@app.get('/saludo')  
+async def root():  
+    return "estoy saludando"
+
+
